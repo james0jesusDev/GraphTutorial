a688cdf307bc16d27e34645d7744c65fa1fd9993
james0jesusDev <james0jesus@gmail.com>
2024-04-22 18:07:43 +0200

    Agregar archivos de proyecto.

---
+++ Alerts/AlertExtensions.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraphTutorial
+{
+    //Metodos de Extension , en este ejemplo al IActionResult
+    public static class AlertExtensions
+    {
+        public static IActionResult WithError(this IActionResult result,
+                                            string message,
+                                            string debugInfo = null)
+        {
+            return Alert(result, "danger", message, debugInfo);
+        }
+
+        public static IActionResult WithSuccess(this IActionResult result,
+                                            string message,
+                                            string debugInfo = null)
+        {
+            return Alert(result, "success", message, debugInfo);
+        }
+
+        public static IActionResult WithInfo(this IActionResult result,
+                                            string message,
+                                            string debugInfo = null)
+        {
+            return Alert(result, "info", message, debugInfo);
+        }
+
+        private static IActionResult Alert(IActionResult result,
+                                        string type,
+                                        string message,
+                                        string debugInfo)
+        {
+            return new WithAlertResult(result, type, message, debugInfo);
+        }
+    }
+}
---
+++ Graph/GraphConstants.cs
@@ -0,0 +1,13 @@
+namespace GraphTutorial
+{
+    public static class GraphConstants
+    {
+        public readonly static string[] Scopes
+            =
+        {
+            "User.Read",
+            "MailboxSettings.Read",
+            "Calendars.ReadWrite"
+        };
+    }
+}
